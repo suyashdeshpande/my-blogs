fb11e93658276d69a97903b2690a26ca80fa3fea
Suyash Deshpande <suyash.champ1@gmail.com>
2020-06-20 01:00:38 +0530

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,6 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: require("./site-meta-data.json"),
-  pathPrefix: "/blogs",
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
