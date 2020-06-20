c8eeb1efd717168d2f540f3b10964c627e1e93f2
Suyash Deshpande <suyash.champ1@gmail.com>
2020-06-20 09:30:34 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-716c80cb81cc4d6fdde0.js"
+    "url": "webpack-runtime-553b80c09789291d348e.js"
   },
   {
-    "url": "styles.57549d926a8ce7aa67e3.css"
+    "url": "styles.1364f3a6addea1d6687c.css"
   },
   {
     "url": "styles-89fd2ae28bdf06750a71.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b4b03bc60f32238c4aae0ab85718a082"
+    "revision": "915df45ddfcfea00afe612da214b35e0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
@@ -57,7 +57,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "e9117355826fbafab9c1e4c0c35c8b65"
+    "revision": "a66ea214c61872da95fc9945ee100093"
   },
   {
     "url": "manifest.webmanifest",
